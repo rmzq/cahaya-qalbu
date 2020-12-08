5cff41e2e337b3f5c16900a667b22c185e25dd77
romzy <onjiromzy@gmail.com>
2020-12-08 11:33:19 +0800

    nambahin bottom tap bar

---
+++ E-Pertanian/app/src/main/java/com/example/e_pertanian/PagerAdapter.java
@@ -0,0 +1,4 @@
+package com.example.e_pertanian;
+
+public class PagerAdapter {
+}
---
+++ E-Pertanian/app/src/main/java/com/example/e_pertanian/schedule/SchedulingActivity.java
@@ -0,0 +1,14 @@
+package com.example.e_pertanian.schedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SchedulingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scheduling);
+    }
+}
