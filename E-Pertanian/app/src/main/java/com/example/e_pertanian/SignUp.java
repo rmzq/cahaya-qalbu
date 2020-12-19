3647ec7ebbc34949b8c06509c9129c1d1af77bbf
romzy <onjiromzy@gmail.com>
2020-12-19 14:57:55 +0800

    perbaikan tampilan login signup

--- E-Pertanian/app/src/main/java/com/example/e_pertanian/SignUp.java
+++ E-Pertanian/app/src/main/java/com/example/e_pertanian/SignUp.java
@@ -42,6 +42,7 @@ public class SignUp extends AppCompatActivity {
         btnSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                finish();
                 startActivity(new Intent(SignUp.this, Login.class));
             }
         });
